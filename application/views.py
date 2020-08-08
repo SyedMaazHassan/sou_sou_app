283c313499376aecfd79d75e39748dc865b7ddcb
SyedMaazHassan <hafizmaazhassan33@gmail.com>
2020-08-08 13:16:52 -0700

    first version

---
+++ application/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
+from django.http import HttpResponse
+from django.http import JsonResponse
+import json
+
+# main page function
+
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    return render(request, 'main.html')
+
+
+# function for signup
+
+def signup(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        l_name = request.POST['l_name']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        context = {
+            "name":name,
+            "l_name":l_name,
+            "email":email,
+            "pass1":pass1,
+            "pass2":pass2,
+        }
+        if pass1==pass2:
+            if User.objects.filter(username=email).exists():
+                print("Email already taken")
+                messages.info(request, "Entered number already in use!")
+                context['border'] = "email" 
+                return render(request, "signup.html", context)
+
+            user = User.objects.create_user(username=email, first_name=name, password=pass1, last_name=l_name)
+            user.save()
+            
+            return redirect("login")
+        else:
+            messages.info(request, "Your pasword doesn't match!")
+            context['border'] = "password"
+            return render(request, "signup.html", context)
+
+
+    
+    return render(request, "signup.html")
+
+
+# function for login
+
+def login(request):
+
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(username=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("index")
+        else:
+            messages.info(request, "Incorrect login details!")
+            return redirect("login")
+    else:
+        return render(request, "login.html")
+
+
+# function for logout
+
+def logout(request):
+    auth.logout(request)
+    return redirect("index")
+
